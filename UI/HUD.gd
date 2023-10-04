d1f7c8fb313903e070a8a82d22558b826a117d83
eileenh8 <eileen.henriquez@icloud.com>
2023-10-03 22:37:22 -0400

    complete

--- UI/HUD.gd
+++ UI/HUD.gd
@@ -3,6 +3,13 @@ extends Control
 var indicator_margin = Vector2(25, 25)
 var indicator_index = 25
 var Indicator = load("res://UI/Indicator.tscn")
+var indicator_scale = Vector2(0.5,0.5)
+var indicator_scale_start = Vector2(0.5,0.5)
+var indicator_scale_target = Vector2(1.5,1.5)
+var indicator_mod = 0.0
+var indicator_mod_start = 0.0
+var indicator_mod_target = 0.5
+
 
 func _ready():
 	update_score()
@@ -29,6 +36,10 @@ func update_lives():
 
 func update_fever():
 	$Fever.value = Global.fever
+
+func breathe():
+	indicator_scale = indicator_scale_target if indicator_scale == indicator_scale_start else indicator_scale_start
+	indicator_mod = indicator_mod_target if indicator_mod == indicator_mod_start else indicator_mod_start
 
 func _on_Timer_timeout():
 	Global.update_time(-1)
